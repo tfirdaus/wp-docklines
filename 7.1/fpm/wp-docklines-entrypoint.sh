eae945acf70efcebdba7fd5f1938fdfc2f12c774
Thoriq Firdaus <thoriqoe@gmail.com>
2017-12-11 16:10:45 +0700

    Update entrypoint script

--- 7.1/fpm/wp-docklines-entrypoint.sh
+++ 7.1/fpm/wp-docklines-entrypoint.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Set the wp-content in a separate directory and symlink it to the actual
+# working directory.
+#
+# This makes it possible to cache cache the directory in the CI like
+# Bitbucket Pipelines and to avoid being overidden when the WordPress
+# files are copied to the workcing directory.
 WP_CONTENT_ROOT=/var/www/wp-content
-WP_WORKDIR=/var/www/html
+WP_CONTENT_WORK=/var/www/html/wp-content
 
 if [[ ! -d "$WP_CONTENT_ROOT" ]]; then
 	echo >&2 "NOTICE: 📁 $WP_CONTENT_ROOT is not exist. Creating the directory now."
@@ -21,7 +27,7 @@ if ! [ -e index.php -a -e wp-includes/version.php ]; then
 		( set -x; ls -A; sleep 10 )
 	fi
 	tar cf - --one-file-system -C /usr/src/wordpress . | tar xf -
-	echo >&2 "NOTICE: ✅ Complete! WordPress has been successfully copied to $PWD"
+	echo >&2 "SUCCESS: ✅ Complete! WordPress has been successfully copied to $PWD"
 	if [ ! -e .htaccess ]; then
 		# NOTE: The "Indexes" option is disabled in the php:apache base image
 		cat > .htaccess <<-'EOF'
@@ -41,19 +47,16 @@ if ! [ -e index.php -a -e wp-includes/version.php ]; then
 fi
 
 # Verify if the wp-content is a symlink and pointing to the correct directory
-if ! [[ -L "$WP_WORKDIR/wp-content" ]] || ! [[ "$(readlink -- "$WP_WORKDIR/wp-content")" = $WP_CONTENT_ROOT ]]; then
-	if [[ -d "$WP_WORKDIR/wp-content" ]]; then
-		rm -r $WP_WORKDIR/wp-content
+if ! [[ -L "$WP_CONTENT_WORK" ]] || ! [[ "$(readlink -- "$WP_CONTENT_WORK")" = $WP_CONTENT_ROOT ]]; then
+	if [[ -d "$WP_CONTENT_WORK" ]]; then
+		rm -r $WP_CONTENT_WORK
 	fi
 
 	# Create a symlink to the /wp-content root directory
-	ln -sfn $WP_CONTENT_ROOT $WP_WORKDIR/wp-content
-
+	ln -sfn $WP_CONTENT_ROOT $WP_CONTENT_WORK
 	if [ $? -eq 0 ]; then
-		echo >&2 "NOTICE: ✅ Symlink to $WP_CONTENT_ROOT is already created."
+		echo >&2 "SUCCESS: ✅ Symlink to $WP_CONTENT_ROOT has been created."
 	fi
-else
-	echo >&2 "NOTICE: ✅ Symlink to $WP_CONTENT_ROOT is already created."
 fi
 
 # Execute the Docker Entrypoint from the WordPress image.
