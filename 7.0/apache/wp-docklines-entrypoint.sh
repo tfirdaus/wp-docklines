12bd7265937faad72c1db7d0042087338dbd8cfe
Thoriq Firdaus <thoriqoe@gmail.com>
2017-12-16 14:27:06 +0700

    Update for better compat with composer

--- 7.0/apache/wp-docklines-entrypoint.sh
+++ 7.0/apache/wp-docklines-entrypoint.sh
@@ -1,65 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Set the wp-content in a separate directory and symlink it to the actual
-# working directory.
-#
-# This makes it possible to cache cache the directory in the CI like
-# Bitbucket Pipelines and to avoid being overidden when the WordPress
-# files are copied to the workcing directory.
-WP_CONTENT_ROOT=/var/www/wp-content
-WP_CONTENT_WORK=/var/www/html/wp-content
+# WordPress working directory.
+WORDPRESS_ROOT=/var/www/html
 
-if [[ ! -d "$WP_CONTENT_ROOT" ]]; then
-	echo >&2 "NOTICE: 📁 $WP_CONTENT_ROOT is not exist. Creating the directory now."
-	mkdir $WP_CONTENT_ROOT
-fi
+# Register WPackagist repository.
+cat > $WORDPRESS_ROOT/composer.json <<-'EOF'
+{"repositories":[{"type":"composer","url":"https://wpackagist.org"}]}
+EOF
 
-# Reset the directory permission
-chown www-data:www-data $WP_CONTENT_ROOT
-
-# Copy the WordPress Core files
-# https://github.com/docker-library/wordpress/blob/master/php5.6/apache/docker-entrypoint.sh#L27-L51
-if ! [ -e index.php -a -e wp-includes/version.php ]; then
-	echo >&2 "NOTICE: 💬 WordPress not found in $PWD - 🔄 copying now..."
-	if [ "$(ls -A)" ]; then
-		echo >&2 "WARNING: ⚠️ $PWD is not empty - press Ctrl+C now if this is an error!"
-		( set -x; ls -A; sleep 10 )
-	fi
-	tar cf - --one-file-system -C /usr/src/wordpress . | tar xf -
-	echo >&2 "SUCCESS: ✅ Complete! WordPress has been successfully copied to $PWD"
-	if [ ! -e .htaccess ]; then
-		# NOTE: The "Indexes" option is disabled in the php:apache base image
-		cat > .htaccess <<-'EOF'
-			# BEGIN WordPress
-			<IfModule mod_rewrite.c>
-			RewriteEngine On
-			RewriteBase /
-			RewriteRule ^index\.php$ - [L]
-			RewriteCond %{REQUEST_FILENAME} !-f
-			RewriteCond %{REQUEST_FILENAME} !-d
-			RewriteRule . /index.php [L]
-			</IfModule>
-			# END WordPress
-		EOF
-		chown www-data:www-data .htaccess
-	fi
-fi
-
-# Verify if the wp-content is a symlink and pointing to the correct directory
-if ! [[ -L "$WP_CONTENT_WORK" ]] || ! [[ "$(readlink -- "$WP_CONTENT_WORK")" = $WP_CONTENT_ROOT ]]; then
-	if [[ -d "$WP_CONTENT_WORK" ]]; then
-		rm -r $WP_CONTENT_WORK
-	fi
-
-	# Create a symlink to the /wp-content root directory
-	ln -sfn $WP_CONTENT_ROOT $WP_CONTENT_WORK
-	if [ $? -eq 0 ]; then
-		echo >&2 "SUCCESS: ✅ Symlink to $WP_CONTENT_ROOT has been created."
-	fi
-fi
+# Set the file permission to the web user.
+chown www-data:www-data $WORDPRESS_ROOT/composer.json
 
 # Execute the Docker Entrypoint from the WordPress image.
 docker-entrypoint.sh "$1"
-
 exec "$@"
--- README.md
+++ README.md
@@ -12,12 +12,14 @@ A custom [Docker](https://www.docker.com/) image for WordPress development. It c
 - [Git](https://git-scm.com/)
 
 ## Usage
-If you'd like to use it on your computer, you'll need to install Docker ([Mac](https://www.docker.com/docker-mac)|[Windows](https://www.docker.com/docker-windows)). The image is available in [Docker Hub](https://hub.docker.com/). Run `docker pull tfirdaus/wp-docklines` command to pull the latest image which default to the `latest` tag or `php7.2-fpm`.
+If you'd like to use it on your computer, you'll need to install Docker ([Mac](https://www.docker.com/docker-mac)|[Windows](https://www.docker.com/docker-windows)). The image is available in [Docker Hub](https://hub.docker.com/). Run `docker pull tfirdaus/wp-docklines` command to pull the latest image which default to the `latest` tag or `php7.2-fpm`. Have a look at the [Wiki](https://github.com/tfirdaus/wp-docklines/wiki) for the usage in details.
 
-More examples on the usage will be available soon in the [Wiki](https://github.com/tfirdaus/wp-docklines/wiki).
+- [Docker Compose](https://github.com/tfirdaus/wp-docklines/wiki/Docker-Compose)
+- Bitbucket Pipelines (soon)
+- SSL & HTTPS (soon)
 
 ## PHP Libraries / Extensions
 Following the image origin at [WordPress Official Docker image](https://hub.docker.com/_/wordpress/), this image does not provide any additional PHP extensions or other libraries as well. There are an infinite number of possible plugins and including every PHP extension that exists would dramatically increase the image size.
 
 ## Why?
-Yes, there are a few Docker images for WordPress development. Yet I found most of these images are stacked with things that are not needed on most of my projects or are not suitable for the workflow. These images are built on top of those necessities; a Development environment with the similar _stacks_ to the Production environment of those websites that I'm working on. Eventually, it's become an opportunity to dig into Docker deeper.
+Yes, there are a few Docker images for WordPress development. Yet I found most of these images are stacked with things that are not needed in my projects, not suitable for my workflow, or it does not work (somehow) with the CI service that I'm using. These images are built on top of those necessities. Eventually, it's also become an opportunity for me to dig into Docker deeper.
