580b6d496dbfe0eeaaf998453ed268e187806f3a
Thoriq Firdaus <thoriqoe@gmail.com>
2017-12-18 08:47:21 +0700

    Update composer

--- 7.2/fpm/wp-docklines-entrypoint.sh
+++ 7.2/fpm/wp-docklines-entrypoint.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# WordPress working directory.
-WORDPRESS_ROOT=/var/www/html
+# Create the Composer Home directory for user.
+mkdir -p /var/www/.composer
 
 # Register WPackagist repository.
-cat > $WORDPRESS_ROOT/composer.json <<-'EOF'
+cat > $(pwd)/composer.json <<-'EOF'
 {"repositories":[{"type":"composer","url":"https://wpackagist.org"}]}
 EOF
 
 # Set the file permission to the web user.
-chown www-data:www-data $WORDPRESS_ROOT/composer.json
+chown www-data:www-data /var/www/.composer
+chown www-data:www-data $(pwd)/composer.json
 
 # Execute the Docker Entrypoint from the WordPress image.
 docker-entrypoint.sh "$1"
